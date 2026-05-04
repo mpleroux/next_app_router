8e4329958832311684a7c36d4d3d724d1283ae60
Michael LeRoux <946718+mpleroux@users.noreply.github.com>
2026-05-03 20:42:31 -0400

    Ch. 12: Add notFound() support

---
+++ app/dashboard/invoices/[id]/edit/not-found.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import { FaceFrownIcon } from "@heroicons/react/24/outline";
+
+export default function NotFound() {
+  return (
+    <main className="gap-2 flex h-full flex-col items-center justify-center">
+      <FaceFrownIcon className="w-10 text-gray-400" />
+      <h2 className="text-xl font-semibold">404 Not Found</h2>
+      <p>Could not find the requested invoice.</p>
+      <Link
+        href="/dashboard/invoices"
+        className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white hover:bg-blue-400 transition-colors">
+        Go Back
+      </Link>
+    </main>
+  );
+}
